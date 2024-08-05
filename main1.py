7479f30618daabf712aab29b7a1106c921390cf4
vxndtt <alenakad16@mail.ru>
2024-08-05 15:58:19 +0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,15 @@
+name = '1st program'
+print(9**0.5*5)
+name = '2nd program'
+print(9.99 > 9.98 and 1000 != 1000.1)
+name = '3rd program'
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+name = '4th program'
+print('123.456')
+print(123.456)
+print(123.456*10)
+print(int(123.456*10))
+number = '1234'
+print(number[3:4])
